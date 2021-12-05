69d1f3c0f2fa63f97d7e76148f2e2b9421339b2f
lucas <lucas.ua.eet@gmail.com>
2021-12-05 19:49:10 +0000

    adds secret temporarly

---
+++ config/secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+  api_token : "nd$C2mJt1wF7Pbq0zjNXpcPfjO2#^#5j",
+  ws_token : "PJw19aHi@d=urEhFjmbd%4xInbWxXQLV",
+}
+
+api token: E@mdHAW=wGZL7IDTUUflXU6bHl=GG9n_
+ws token: K^Mk=84YaaYe=*xXEf-mD-@uHcdd_1BN
