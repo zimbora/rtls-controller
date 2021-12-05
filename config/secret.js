36047ac1f9d2b602bea1f33595656c6181a7f7c8
lucas <lucas.ua.eet@gmail.com>
2021-12-05 19:55:18 +0000

    removing secret file

--- config/secret.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  api_token : "nd$C2mJt1wF7Pbq0zjNXpcPfjO2#^#5j",
-  ws_token : "PJw19aHi@d=urEhFjmbd%4xInbWxXQLV",
-}
-
-api token: E@mdHAW=wGZL7IDTUUflXU6bHl=GG9n_
-ws token: K^Mk=84YaaYe=*xXEf-mD-@uHcdd_1BN
